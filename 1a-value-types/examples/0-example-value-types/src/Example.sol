97307b5637cde8bdf55e035e374ba718a6a96b65
ogweirdo <ogbandav102@gmail.com>
2023-12-25 22:05:59 +0100

    practiced on data types..

--- 1a-value-types/examples/0-example-value-types/src/Example.sol
+++ 1a-value-types/examples/0-example-value-types/src/Example.sol
@@ -5,8 +5,7 @@ import "forge-std/console.sol";
 
 contract Example {
     uint8 a = 255; // 0 -> 255
-    uint256 b = 22; // alias: uint
-
+    uint256 b = type(uint256).max; // alias: uint
     int8 c = 127; // -128 -> 127
     int256 d = -55; // alias: int256
 
@@ -19,5 +18,34 @@ contract Example {
         Right
     }
 
-    Choice choice = Choice.Up;
+    constructor(Choice choice){
+        unchecked {
+        a +=1;
+        a +=1;
+        a +=1;
+        }
+        // console.log(a); // logs the value of a
+        // console.logBytes32(bytes32(b)); // logs he value of b in bytes
+        // console.logInt(type(int8).max); 
+        // console.logInt(type(int8).min); 
+
+        if(myCondition){
+            console.log("thank you Jesus");
+        }
+
+        if(choice == Choice.Up){
+            console.log("up");
+        }
+
+        if(choice = Choice.Left){
+            console.log("left!!!!!!!!!!!");
+        }
+
+    }
+
+    
+
+    
+    
+
 }
